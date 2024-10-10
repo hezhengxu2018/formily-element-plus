36f6e314b6324237c94d3588b49ba6f51b39e3e2
hezhengxu <hzx.tony@hotmail.com>
2024-10-10 20:23:57 +0800

    chore(dependency): 添加组件测试框架

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '.',
-  plugins: [vue()],
+  plugins: [vue(), vueJsx()],
+  test: {
+    css: true,
+    browser: {
+      provider: 'playwright', // or 'webdriverio'
+      enabled: true,
+      name: 'chromium', // browser name is required
+    },
+  },
 })
