b9dc0cd44fe93ccf9da1ee615574409c88b7f6ea
hezhengxu <hzx.tony@hotmail.com>
2024-12-30 16:37:02 +0800

    fix(select-table): 修复select-table交互异常及数据反显异常
    修复选择数据项数量显示异常的问题
    修复初始化数据无法反显的问题
    修复取消全选时触发事件异常的问题

--- src/select-table/index.ts
+++ src/select-table/index.ts
@@ -4,12 +4,22 @@ import type { Column, TableInstance, TableProps } from 'element-plus'
 import type { Component, PropType } from 'vue'
 import { isArr, isFn } from '@formily/shared'
 import { connect, mapProps, useFieldSchema } from '@formily/vue'
-import { ElRadio, ElRadioGroup, ElTable, ElTableColumn, version } from 'element-plus'
-import { differenceWith, remove, uniqWith } from 'lodash-es'
+import {
+  ElLink,
+  ElRadio,
+  ElRadioGroup,
+  ElTable,
+  ElTableColumn,
+  version,
+  vLoading,
+} from 'element-plus'
+import { differenceWith, isNil, remove, uniqWith } from 'lodash-es'
 
 import { gt } from 'semver'
-import { defineComponent, h, nextTick, ref, watch } from 'vue'
-import { composeExport, stylePrefix } from '../__builtins__'
+import { computed, defineComponent, h, nextTick, ref, watch, withDirectives } from 'vue'
+import { stylePrefix } from '../__builtins__/configs'
+import { composeExport } from '../__builtins__/shared'
+import './style.ts'
 
 type IFilterOption = boolean | ((option: any, keyword: string) => boolean)
 
@@ -42,63 +52,6 @@ interface ObservableColumnSource {
 function isColumnComponent(schema: Schema) {
   return schema['x-component']?.indexOf('Column') > -1
 }
-
-// function getArrayTableColumns(sources: ObservableColumnSource[]): ColumnProps[] {
-//   return sources.reduce(
-//     (
-//       buf: ColumnProps[],
-//       { name, columnProps, schema, display, required },
-//       key,
-//     ) => {
-//       const { title, asterisk, ...props } = columnProps
-//       if (display !== 'visible')
-//         return buf
-//       if (!isColumnComponent(schema))
-//         return buf
-
-//       const render = (startIndex?: Ref<number>) => {
-//         return columnProps?.type && columnProps?.type !== 'default'
-//           ? undefined
-//           : (props: {
-//             row: Record<string, any>
-//             column: ElColumnProps
-//             $index: number
-//           }): VNode => {
-//             const index = (startIndex?.value ?? 0) + props.$index
-//             // const index = reactiveDataSource.value.indexOf(props.row)
-//             const children = h(
-//               ArrayBase.Item,
-//               { index, record: props.row, key: `${key}${index}` },
-//               {
-//                 default: () => {
-//                   return h(
-//                     RecursionField as any,
-//                     {
-//                       schema,
-//                       name: index,
-//                       onlyRenderProperties: true,
-//                     },
-//                     {},
-//                   )
-//                 },
-//               },
-//             )
-//             return children
-//           }
-//       }
-
-//       return (buf as any).concat({
-//         label: title,
-//         ...props,
-//         key,
-//         prop: name,
-//         asterisk: asterisk ?? required,
-//         render,
-//       })
-//     },
-//     [],
-//   )
-// }
 
 function useSchemaColumns() {
   const schema = useFieldSchema().value
@@ -188,208 +141,301 @@ function compatibleRadioValue(key: string) {
   return gt(version, '2.6.0') ? { value: key } : { label: key }
 }
 
-const InnerSelectTable
-  = defineComponent({
-    name: 'FSelectTable',
-    props: {
-      columns: {
-        type: Array as PropType<Column<any>[]>,
-      },
-      mode: {
-        type: String as PropType<ISelectTableProps['mode']>,
-        default: 'multiple',
-      },
-      optionAsValue: {
-        type: Boolean,
-        default: false,
-      },
-      valueType: {
-        type: String as PropType<ISelectTableProps['valueType']>,
-        default: 'all',
-      },
-      loading: {
-        type: Boolean,
-      },
-      primaryKey: {
-        type: [String, Function] as PropType<ISelectTableProps['primaryKey']>,
-      },
-      onSearch: {
-        type: Function as PropType<ISelectTableProps['onSearch']>,
-      },
-      value: {
-        type: [String, Number, Array] as PropType<ISelectTableProps['value']>,
-      },
-      rowKey: {
-        type: [String, Function] as PropType<ISelectTableProps['rowKey']>,
-      },
-      dataSource: {
-        type: Array as PropType<ISelectTableProps['dataSource']>,
-      },
-      clickRowToSelect: {
-        type: Boolean,
-        default: true,
-      },
+const InnerSelectTable = defineComponent({
+  name: 'FSelectTable',
+  props: {
+    columns: {
+      type: Array as PropType<Column<any>[]>,
     },
-    emits: ['change'],
-    setup(props, { emit, attrs }) {
-      const elTableRef = ref<TableInstance>()
-      const rowKey = props.rowKey ?? props.primaryKey
-      const selectedFlatDataSource = ref([])
-      // 为了获取移除的项而缓存的当前页面的前一次选择
-      let prevSelection = []
+    mode: {
+      type: String as PropType<ISelectTableProps['mode']>,
+      default: 'multiple',
+    },
+    optionAsValue: {
+      type: Boolean,
+      default: false,
+    },
+    valueType: {
+      type: String as PropType<ISelectTableProps['valueType']>,
+      default: 'all',
+    },
+    primaryKey: {
+      type: [String, Function] as PropType<ISelectTableProps['primaryKey']>,
+    },
+    onSearch: {
+      type: Function as PropType<ISelectTableProps['onSearch']>,
+    },
+    value: {
+      type: [String, Number, Array, Object] as PropType<
+        ISelectTableProps['value']
+      >,
+    },
+    rowKey: {
+      type: [String, Function] as PropType<ISelectTableProps['rowKey']>,
+    },
+    dataSource: {
+      type: Array as PropType<ISelectTableProps['dataSource']>,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    clickRowToSelect: {
+      type: Boolean,
+      default: true,
+    },
+    showAlertToolbar: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  emits: ['change'],
+  setup(props, { emit, attrs, slots }) {
+    const elTableRef = ref<TableInstance>()
+    const rowKey = props.rowKey ?? props.primaryKey
+    function getRowKey(item: unknown) {
+      return isFn(rowKey) ? rowKey(item) : item[rowKey]
+    }
+    const initialSelectedList = props.value?.map((item) => {
+      if (!props.optionAsValue && !isFn(rowKey)) {
+        return {
+          [rowKey]: item,
+        }
+      }
+      return item
+    },
+    )
+    const selectedFlatDataSource = ref(initialSelectedList ?? [])
+    // 为了获取移除的项而缓存的当前页面的前一次选择
+    let prevSelection = []
 
-      const radioSelectedKey = ref()
+    const radioSelectedKey = ref()
 
-      const columnSource = useSchemaColumns()
-      const columns = props.columns ?? useColumns(columnSource)
+    const columnSource = useSchemaColumns()
+    const columns = props.columns ?? useColumns(columnSource)
 
-      // 当前页面是否是树形表格
-      // const isTree = computed(() => {
-      //   const treeProps = attrs.treeProps as TreeProps
-      //   if (treeProps?.children) {
-      //     return props.dataSource.some(item => item[treeProps.children])
-      //   }
-      //   return props.dataSource.some(item => item.children)
-      // })
+    const currentSelectLength = computed(() => {
+      if (props.mode === 'multiple') {
+        return Array.isArray(props.value) ? props.value?.length : 1
+      }
+      else {
+        return radioSelectedKey.value ? 1 : 0
+      }
+    })
 
-      watch(() => props.dataSource, async () => {
-        const selectedKeys = new Set(selectedFlatDataSource.value.map(item => isFn(rowKey) ? rowKey(item) : item[rowKey]))
+    // 当前页面是否是树形表格
+    // const isTree = computed(() => {
+    //   const treeProps = attrs.treeProps as TreeProps
+    //   if (treeProps?.children) {
+    //     return props.dataSource.some(item => item[treeProps.children])
+    //   }
+    //   return props.dataSource.some(item => item.children)
+    // })
+
+    watch(
+      () => props.dataSource,
+      async () => {
+        if (isNil(props.dataSource)) {
+          return
+        }
+        const selectedKeys = new Set(
+          selectedFlatDataSource.value.map(item => getRowKey(item)),
+        )
         await nextTick()
         for (const item of props.dataSource) {
-          if (selectedKeys.has(isFn(rowKey) ? rowKey(item) : item[rowKey])) {
-            elTableRef.value.toggleRowSelection(item, true)
+          if (selectedKeys.has(getRowKey(item))) {
+            if (props.mode === 'multiple') {
+              elTableRef.value.toggleRowSelection(item, true)
+            }
+            else {
+              elTableRef.value.setCurrentRow(item)
+              onRadioClick(item)
+            }
           }
+          await nextTick()
+          prevSelection = elTableRef.value.getSelectionRows()
         }
-        await nextTick()
-        prevSelection = elTableRef.value.getSelectionRows()
-      })
+      },
+      { immediate: true },
+    )
 
-      function onSelect(newSelection: Record<string, any>[]) {
-        console.log(newSelection)
-        const rowKey = props.rowKey ?? props.primaryKey
-        if (!rowKey) {
-          throw new Error('rowKey is required')
-        }
-
-        if (props.mode === 'multiple') {
-          const removedItemList = prevSelection.length > newSelection.length
-            ? differenceWith(prevSelection, newSelection, (itemPrev, itemNext) => {
-                return isFn(rowKey) ? rowKey(itemPrev) === rowKey(itemNext) : itemPrev[rowKey] === itemNext[rowKey]
-              })
-            : []
-          prevSelection = newSelection
-          selectedFlatDataSource.value = uniqWith([...selectedFlatDataSource.value, ...newSelection], (itemPrev, itemNext) => {
-            return isFn(rowKey) ? rowKey(itemPrev) === rowKey(itemNext) : itemPrev[rowKey] === itemNext[rowKey]
-          })
-          if (removedItemList.length > 0) {
-            const removedKeys = new Set(removedItemList.map(item => isFn(rowKey) ? rowKey(item) : item[rowKey]))
-            remove(selectedFlatDataSource.value, item => removedKeys.has(isFn(rowKey) ? rowKey(item) : item[rowKey]))
-          }
-
-          if (props.optionAsValue) {
-            emit('change', selectedFlatDataSource.value)
-          }
-          else {
-            const selectedKeys = selectedFlatDataSource.value.map(item => isFn(rowKey) ? rowKey(item) : item[rowKey])
-            emit('change', selectedKeys)
-          }
-        }
-        // if (readPretty || disabled || readOnly || record?.disabled) {
-        //   return
-        // }
-        // const selectedRowKey = props.primaryKey ? record?.[props.primaryKey] : null
-        // const isSelected = selectedRowKey
-        //   ? selected?.includes(selectedRowKey)
-        //   : false
-        // let selectedRowKeys: (string | null)[] = []
-        // if (mode === 'single' && selectedRowKey) {
-        //   selectedRowKeys = [selectedRowKey]
-        // }
-        // else {
-        //   selectedRowKeys = isSelected ? selected.filter(item => item !== selectedRowKey) : [...selected, selectedRowKey]
-        // }
-        // if (rowSelection?.checkStrictly === false) {
-        //   onSlacklyChange(selectedRowKeys)
-        // }
-        // else {
-        //   onInnerChange(selectedRowKeys)
-        // }
+    function onSelect(newSelection: Record<string, any>[]) {
+      const rowKey = props.rowKey ?? props.primaryKey
+      if (!rowKey) {
+        throw new Error('rowKey is required')
       }
 
-      function onRowClick(row: Record<string, any>, _, event: Event) {
-        if (!props.clickRowToSelect)
-          return
-
-        if (props.mode === 'multiple') {
-          const checkboxDOM = (event.target as Element).closest('tr').querySelector('input[type="checkbox"]')
-          if (checkboxDOM instanceof HTMLElement) {
-            checkboxDOM.click()
-          }
-        }
-        else {
-          const radioDOM = (event.target as Element).closest('tr').querySelector('input[type="radio"]')
-          if (radioDOM instanceof HTMLElement) {
-            radioDOM.click()
-          }
-        }
+      if (props.mode === 'single') {
+        return
+      }
+      const removedItemList
+        = prevSelection.length > newSelection.length
+          ? differenceWith(
+              prevSelection,
+              newSelection,
+              (itemPrev, itemNext) => {
+                return isFn(rowKey)
+                  ? rowKey(itemPrev) === rowKey(itemNext)
+                  : itemPrev[rowKey] === itemNext[rowKey]
+              },
+            )
+          : []
+      prevSelection = [...newSelection]
+      selectedFlatDataSource.value = uniqWith(
+        [...selectedFlatDataSource.value, ...newSelection],
+        (itemPrev, itemNext) => {
+          return isFn(rowKey)
+            ? rowKey(itemPrev) === rowKey(itemNext)
+            : itemPrev[rowKey] === itemNext[rowKey]
+        },
+      )
+      if (removedItemList.length > 0) {
+        const removedKeys = new Set(
+          removedItemList.map(item => getRowKey(item)),
+        )
+        remove(selectedFlatDataSource.value, item =>
+          removedKeys.has(isFn(rowKey) ? rowKey(item) : item[rowKey]))
       }
 
-      function onClearSelectionClick() {
+      if (props.optionAsValue) {
+        emit('change', selectedFlatDataSource.value)
+      }
+      else {
+        const selectedKeys = selectedFlatDataSource.value.map(item =>
+          isFn(rowKey) ? rowKey(item) : item[rowKey],
+        )
+        emit('change', selectedKeys)
+      }
+    }
+
+    function onRadioClick(row) {
+      const finalKey = getRowKey(row)
+      radioSelectedKey.value = finalKey
+      if (props.optionAsValue) {
+        emit('change', row)
+      }
+      else {
+        emit('change', finalKey)
+      }
+    }
+
+    function onRowClick(row: Record<string, any>, _, event: Event) {
+      if (!props.clickRowToSelect)
+        return
+
+      if (props.mode === 'multiple') {
+        const checkboxDOM = (event.target as Element)
+          .closest('tr')
+          .querySelector('input[type="checkbox"]')
+        if (checkboxDOM instanceof HTMLElement) {
+          checkboxDOM.click()
+        }
+      }
+      else {
+        const radioDOM = (event.target as Element)
+          .closest('tr')
+          .querySelector('input[type="radio"]')
+        if (radioDOM instanceof HTMLElement) {
+          radioDOM.click()
+        }
+      }
+    }
+
+    function onClearSelectionClick() {
+      if (props.mode === 'multiple') {
         elTableRef.value.clearSelection()
         emit('change', [])
         selectedFlatDataSource.value = []
       }
+      else {
+        radioSelectedKey.value = null
+        emit('change', null)
+      }
+    }
 
-      return () => h('div', { class: `${stylePrefix}-select-table` }, [props.value?.length > 0 && h('div', { class: `${stylePrefix}-select-table-alert-container` }, [
-        h('span', `已选择 ${props.value?.length} 项 `),
-        h(ElLink, { type: 'primary', underline: false, style: 'margin-left: 8px;', onClick: onClearSelectionClick }, () => '取消选择'),
-      ]), h(
-        ElTable,
-        {
-          ref(ref) {
-            elTableRef.value = ref as TableInstance
-          },
-          ...props,
-          ...attrs,
-          rowClassName: props.clickRowToSelect ? `--click-row-select` : '',
-          data: props.dataSource,
-          type: 'selection',
-          onSelect,
-          onSelectAll: onSelect,
-          onRowClick,
-        },
-        () => {
-          return [props.mode === 'multiple'
-            ? h(ElTableColumn, { type: 'selection' })
-            : h(ElTableColumn, { type: 'radio', width: 46 }, {
-                default({ row }) {
-                  const finalKey = isFn(rowKey) ? rowKey(row) : row[rowKey]
-                  return h(ElRadioGroup, { modelValue: radioSelectedKey.value }, {
-                    default() {
-                      return h(ElRadio, {
-                        ...compatibleRadioValue(finalKey),
-                        onChange: () => {
-                          radioSelectedKey.value = finalKey
-                          if (props.optionAsValue) {
-                            emit('change', row)
-                          }
-                          else {
-                            emit('change', finalKey)
-                          }
+    return () =>
+      h('div', { class: `${stylePrefix}-select-table` }, [
+        currentSelectLength.value > 0
+        && props.showAlertToolbar
+        && h('div', { class: `${stylePrefix}-select-table-alert-container` }, [
+          h(
+            'span',
+            `已选择 ${currentSelectLength.value} 项 `,
+          ),
+          h(
+            ElLink,
+            {
+              type: 'primary',
+              underline: false,
+              style: 'margin-left: 8px;',
+              onClick: onClearSelectionClick,
+            },
+            () => '取消选择',
+          ),
+        ]),
+        withDirectives(
+          h(
+            ElTable,
+            {
+              ref(ref) {
+                elTableRef.value = ref as TableInstance
+              },
+              ...props,
+              ...attrs,
+              rowClassName: props.clickRowToSelect ? `--click-row-select` : '',
+              data: props.dataSource,
+              type: 'selection',
+              onSelect,
+              onSelectAll: onSelect,
+              onRowClick,
+            },
+            () => {
+              return [
+                props.mode === 'multiple'
+                  ? h(ElTableColumn, { type: 'selection' })
+                  : h(
+                      ElTableColumn,
+                      { type: 'radio', width: 46 },
+                      {
+                        default({ row }) {
+                          const finalKey = getRowKey(row)
+                          return h(
+                            ElRadioGroup,
+                            { modelValue: radioSelectedKey.value },
+                            {
+                              default() {
+                                return h(
+                                  ElRadio,
+                                  {
+                                    ...compatibleRadioValue(finalKey),
+                                    onChange: () => onRadioClick(row),
+                                  },
+                                  () => '',
+                                )
+                              },
+                            },
+                          )
                         },
-                      }, () => '')
-                    },
-                  })
-                },
-              }), columns.map((colItem) => {
-            return h(ElTableColumn, { ...colItem })
-          })]
-        },
-      )])
-    },
-  })
+                      },
+                    ),
+                columns.length === 0
+                  ? slots?.default?.()
+                  : columns.map((colItem) => {
+                      return h(ElTableColumn, { ...colItem })
+                    }),
+              ]
+            },
+          ),
+          [[vLoading, props.loading]],
+        ),
+      ])
+  },
+})
 
-const SelectTableInner = connect(InnerSelectTable, mapProps({ dataSource: 'dataSource' }))
+const SelectTableInner = connect(
+  InnerSelectTable,
+  mapProps({ dataSource: 'dataSource', loading: 'loading' }),
+)
 
 const SelectTableColumn: Component = {
   name: 'FArrayTableColumn',
