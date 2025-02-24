d8874c6af47159e23eb7e1e5a390469da0ed7d0e
hezhengxu <hzx.tony@hotmail.com>
2025-02-24 17:39:44 +0800

    feat(time-select): 新增时间选择组件

---
+++ src/time-select/index.ts
@@ -0,0 +1,20 @@
+import type { TimeSelectProps } from 'element-plus'
+import { connect, mapProps, mapReadPretty } from '@formily/vue'
+import { ElTimeSelect } from 'element-plus'
+import { transformComponent } from '../__builtins__'
+import { PreviewText } from '../preview-text'
+
+const TransformElTimeSelect = transformComponent<TimeSelectProps>(
+  ElTimeSelect,
+  {
+    change: 'update:modelValue',
+  },
+)
+
+export const TimeSelect = connect(
+  TransformElTimeSelect,
+  mapProps({ value: 'modelValue', disabled: 'disabled' }),
+  mapReadPretty(PreviewText.Input),
+)
+
+export default TimeSelect
