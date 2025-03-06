5491570d8c0ca73a4fa19f73b884f03b28b165d4
hezhengxu <hzx.tony@hotmail.com>
2025-03-06 15:13:18 +0800

    fix(checkbox): checkbox添加对低版本element-plus的兼容

--- src/checkbox/checkbox-option.vue
+++ src/checkbox/checkbox-option.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import type { CheckboxProps } from 'element-plus'
 import type { PropType } from 'vue'
-import { ElCheckbox, ElCheckboxButton } from 'element-plus'
+import { ElCheckbox, ElCheckboxButton, version } from 'element-plus'
+import { lt } from 'semver'
+import { computed } from 'vue'
 
 defineOptions({
   name: 'FCheckboxOption',
@@ -13,29 +15,48 @@ const props = defineProps({
   optionType: String as PropType<'default' | 'button'>,
 })
 const emits = defineEmits(['change'])
+const IS_LESS_THAN_2_6_0 = lt(version, '2.6.0')
+const compatiableProps = computed(() => {
+  if (props.option === undefined)
+    return {}
+  if (IS_LESS_THAN_2_6_0) {
+    return {
+      ...props.option,
+      label: props.option.value,
+    }
+  }
+  return props.option
+})
 </script>
 
 <template>
   <template v-if="props.optionType === 'button'">
     <ElCheckboxButton
-      v-bind="props.option"
+      v-bind="compatiableProps"
       @update:model-value="(value) => emits('change', value)"
     >
-      <slot />
+      <template v-if="$slots.default">
+        <slot />
+      </template>
+      <template v-else>
+        {{ props.option.label }}
+      </template>
     </ElCheckboxButton>
   </template>
   <template v-else>
     <ElCheckbox
       v-if="$slots.default"
-      v-bind="props.option"
+      v-bind="compatiableProps"
       @update:model-value="(value) => emits('change', value)"
     >
       <slot />
     </ElCheckbox>
     <ElCheckbox
       v-else
-      v-bind="props.option"
+      v-bind="compatiableProps"
       @update:model-value="(value) => emits('change', value)"
-    />
+    >
+      {{ props.option?.label }}
+    </ElCheckbox>
   </template>
 </template>
