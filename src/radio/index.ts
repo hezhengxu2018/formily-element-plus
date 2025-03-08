d8413d3c9cc22f2e67c6a38052f59478f1ddf4c7
hezhengxu <hzx.tony@hotmail.com>
2025-03-08 23:49:42 +0800

    refactor(Radio): 移除对Radio的封装

--- src/radio/index.ts
+++ src/radio/index.ts
@@ -1,29 +1,16 @@
-import type { RadioProps } from 'element-plus'
 import { connect, mapProps, mapReadPretty } from '@formily/vue'
 import { ElRadio } from 'element-plus'
-import { composeExport, transformComponent } from '../__builtins__/shared'
+import { composeExport } from '../__builtins__/shared'
 import { PreviewText } from '../preview-text'
 import FRadioGroup from './radio-group.vue'
 import './style.scss'
-
-const TransformElRadio = transformComponent<RadioProps>(ElRadio, {
-  change: 'update:modelValue',
-})
-
-const InnerRadio = connect(
-  TransformElRadio,
-  mapProps({
-    value: 'modelValue',
-    disabled: true,
-  }),
-)
 
 const RadioGroup = connect(
   FRadioGroup,
   mapProps({ dataSource: 'options', disabled: true }),
   mapReadPretty(PreviewText.Select),
 )
-export const Radio = composeExport(InnerRadio, {
+export const Radio = composeExport(ElRadio, {
   Group: RadioGroup,
 })
 
