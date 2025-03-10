dccbb96badbcfb90770a580ab19cc12243130e6f
hezhengxu <hzx.tony@hotmail.com>
2025-03-10 20:18:50 +0800

    refactor(reset): 重构Reset组件

--- src/reset/index.ts
+++ src/reset/index.ts
@@ -1,51 +1,6 @@
-import type { IFieldResetOptions } from '@formily/core'
-import type { ElButton as ElButtonProps } from 'element-plus'
 import { observer } from '@formily/reactive-vue'
-import { useParentForm } from '@formily/vue'
+import FReset from './reset.vue'
 
-import { ElButton } from 'element-plus'
-import { defineComponent, h } from 'vue'
-
-export type ResetProps = IFieldResetOptions & typeof ElButtonProps
-
-export const Reset = observer(
-  defineComponent({
-    name: 'FReset',
-    props: {
-      forceClear: {
-        type: Boolean,
-        default: false,
-      },
-      validate: {
-        type: Boolean,
-        default: false,
-      },
-    },
-    setup(props, { attrs, slots }: any) {
-      const formRef = useParentForm()
-      return () => {
-        const form = formRef?.value
-        return h(
-          ElButton,
-          {
-            ...attrs,
-            onClick: (e: MouseEvent) => {
-              if (attrs?.onClick && attrs.onClick(e) === false)
-                return
-              form
-                ?.reset('*', {
-                  forceClear: props.forceClear,
-                  validate: props.validate,
-                })
-                .then(attrs.onResetValidateSuccess as (e: any) => void)
-                .catch(attrs.onResetValidateFailed as (e: any) => void)
-            },
-          },
-          slots,
-        )
-      }
-    },
-  }),
-)
+export const Reset = observer(FReset)
 
 export default Reset
