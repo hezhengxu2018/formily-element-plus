e995b0957f85938b9f1003c75a91faa42f5566e2
hezhengxu <hzx.tony@hotmail.com>
2025-04-17 17:53:09 +0800

    test(form-layout): 完善form-layout组件单元测试

--- src/form-layout/types.ts
+++ src/form-layout/types.ts
@@ -1,7 +1,7 @@
 export interface IFormLayoutProps {
   colon?: boolean
-  labelAlign?: 'right' | 'left'
-  wrapperAlign?: 'right' | 'left'
+  labelAlign?: 'right' | 'left' | ('right' | 'left')[]
+  wrapperAlign?: 'right' | 'left' | ('right' | 'left')[]
   labelWrap?: boolean
   labelWidth?: number
   wrapperWidth?: number
@@ -15,6 +15,30 @@ export interface IFormLayoutProps {
     | 'horizontal'
     | 'inline'
     | ('vertical' | 'horizontal' | 'inline')[]
+  feedbackLayout?: 'loose' | 'terse' | 'popover'
+  tooltipLayout?: 'icon' | 'text'
+  breakpoints?: number[]
+  shallow?: boolean
+  // element-plus form props
+  hideRequiredAsterisk?: boolean
+  statusIcon?: boolean
+  requireAsteriskPosition?: 'left' | 'right'
+}
+
+
+export interface ICalculatedFormLayoutProps {
+  colon?: boolean
+  labelAlign?: 'right' | 'left'
+  wrapperAlign?: 'right' | 'left'
+  labelWrap?: boolean
+  labelWidth?: number | string
+  wrapperWidth?: number | string
+  wrapperWrap?: boolean
+  labelCol?: number
+  wrapperCol?: number
+  fullness?: boolean
+  size?: 'small' | 'default' | 'large'
+  layout?: 'vertical' | 'horizontal' | 'inline'
   feedbackLayout?: 'loose' | 'terse' | 'popover'
   tooltipLayout?: 'icon' | 'text'
   breakpoints?: number[]
