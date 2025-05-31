bfe627dadc026f6fad566a05535dce9d492f3e0a
Tony <hzx.tony@hotmail.com>
2025-05-31 21:46:41 +0800

    feat(array-table): fork pagination from element-plus
    拷贝一份属于自己的分页组件，方便添加报错信息

---
+++ src/pagination/components/total.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { usePagination } from 'element-plus/es/components/pagination/src/usePagination'
+import { useLocale, useNamespace } from 'element-plus/es/hooks/index'
+import { paginationTotalProps } from './total'
+
+defineOptions({
+  name: 'ElPaginationTotal',
+})
+defineProps(paginationTotalProps)
+const { t } = useLocale()
+const ns = useNamespace('pagination')
+const { disabled } = usePagination()
+</script>
+
+<template>
+  <span :class="ns.e('total')" :disabled="disabled">
+    {{
+      t('el.pagination.total', {
+        total,
+      })
+    }}
+  </span>
+</template>
