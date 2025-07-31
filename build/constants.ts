44583009ef0d1a13268535a7a6f0ef3a73fdd7b6
hezhengxu <hzx.tony@hotmail.com>
2025-07-31 11:03:38 +0800

    fix(form-layout): 修复组件异常的attrs

--- build/constants.ts
+++ build/constants.ts
@@ -1,6 +1,6 @@
+import { readFileSync } from 'node:fs'
 import path from 'node:path'
 import process from 'node:process'
-import fs from 'fs-extra'
 
 export const cwd = process.cwd()
-export const pkg = fs.readJSONSync(path.resolve(cwd, 'package.json'))
+export const pkg = JSON.parse(readFileSync(path.resolve(cwd, 'package.json'), 'utf8'))
